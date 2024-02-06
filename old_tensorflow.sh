a84db739c5b9b928a6562605d46c7dbcc635811f
River <tziony.i@gmail.com>
2024-02-06 11:00:50 +0200

    added scripts to switch between versions

---
+++ old_tensorflow.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define an associative array to store packages and versions
+declare -A packages=(
+    ["tensorflow"]="1.8.0"
+    ["keras"]="2.1.6"
+    ["h5py"]="2.7.1"
+    # Add more packages as needed
+)
+
+# Iterate over the array and install each package with the specified version
+for package in "${!packages[@]}"; do
+    version=${packages[${package}]}
+
+    echo "Installing ${package} version ${version}..."
+    pip3.6 install --force-reinstall ${package}==${version}
+
+    # Check if the installation was successful
+    if [ $? -eq 0 ]; then
+        echo "Installation of ${package} version ${version} successful."
+    else
+        echo "Installation of ${package} version ${version} failed. Please check the error messages above."
+    fi
+done
