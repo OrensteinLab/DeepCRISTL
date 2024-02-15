db12dbedefe0ca54789895ddf559a9306a8047f0
River <tziony.i@gmail.com>
2024-02-15 08:09:16 +0200

    Fixes to the main TL table

--- fix_supp.py
+++ fix_supp.py
@@ -1,16 +1,25 @@
 import pandas as pd
+import os
 
 BASE_PATH = 'data/main_dataframes/'
-CORRECTION_FILES = ['hart2016-Rpe1Avg.scores.tab',
-                    'hart2016-Hct1162lib1Avg.scores.tab',
-                    'hart2016-HelaLib2Avg.scores.tab',
-                    'hart2016-HelaLib1Avg.scores.tab',
-                    'doench2016_hg19.scores.tab']
+CORRECTION_FOLDER = 'data/main_dataframes/CrisprOR fixes/'
+
+
+# EXPERIMENTS_TO_FIX = ['doench2016_hg19'
+    
+#     'hart2016-Rpe1Avg',
+#                         'hart2016-Hct1162lib1Avg',
+#                         'hart2016-HelaLib2Avg',
+#                         'hart2016-HelaLib1Avg',
+#                         ]
+
+
+
 
 MAIN_FILE = '13059_2016_1012_MOESM14_ESM.tsv'
 
 
-CORRECTION_FILES = [BASE_PATH + file for file in CORRECTION_FILES]
+#CORRECTION_FILES = [CORRECTION_FOLDER + file for file in CORRECTION_FILES]
 MAIN_FILE = BASE_PATH + MAIN_FILE
 
 
@@ -28,15 +37,51 @@ def fix_row_length(file):
         for line in lines:
             f.write('\t'.join(line) + '\n')
 
+    return
+
     
+def fix_ghandi():
+    # open main file and fix the ghandi experiment
+    with open(MAIN_FILE, 'r') as f:
+        lines = f.readlines()
+        lines = [line.strip().split('\t') for line in lines]
+        for line in lines:
+            if line[0] == 'gandhi2016_ci2':
+                line[0] = 'ghandi2016_ci2'
+    
+    with open(MAIN_FILE, 'w') as f:
+        for line in lines:
+            f.write('\t'.join(line) + '\n')
+    
+    return
 
+def get_experiments_to_fix():
+    # open the main file
+    bad_experiments = set()
+    with open(MAIN_FILE, 'r') as f:
+        lines = f.readlines()
+        lines = [line.strip().split('\t') for line in lines]
+        for line in lines[1:]:
+            if len(line) != 21:
+                bad_experiments.add(line[0])
 
-
+    print(bad_experiments)
+    return bad_experiments
 
 def main():
+    # fix the ghandi experiment name
+    fix_ghandi() 
+
+    # Get all the experiments that have a wrong number of columns
+    bad_experiments = get_experiments_to_fix()
+    correction_files = [''.join([CORRECTION_FOLDER,experiment, '.scores.tab']) for experiment in bad_experiments]
+    # Check they all exist
+    for file in correction_files:
+        if not os.path.exists(file):
+            print(f'File {file} does not exist')
     fix_row_length(MAIN_FILE)
     main_df = pd.read_csv(MAIN_FILE, sep='\t')
-    fixes_dfs = [pd.read_csv(file, sep='\t') for file in CORRECTION_FILES]
+    fixes_dfs = [pd.read_csv(file, sep='\t') for file in correction_files]
 
     # for reach row in main_df, find the corresponding row in each fixes_df
     # and update the main_df row with the new values
