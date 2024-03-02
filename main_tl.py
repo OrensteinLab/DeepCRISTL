32fbd2da345d57c2d7ceef9bc628a65120e39c07
River <tziony.i@gmail.com>
2024-03-02 21:31:21 +0200

    added batch simulation

--- main_tl.py
+++ main_tl.py
@@ -77,58 +77,63 @@ if __name__ == '__main__':
 
 
     if config.simulation_type == 'full_sim':
-        print(f'Running full simulation for {config.tl_data} dataset')
+        if config.tl_data == 'U6':
+            datasets = ['chari2015Train293T','doench2014-Hs','doench2014-Mm','doench2016_hg19','hart2016-Hct1162lib1Avg','hart2016-HelaLib1Avg','hart2016-HelaLib2Avg','hart2016-Rpe1Avg','morenoMateos2015','xu2015TrainHl60','xu2015TrainKbm7']
+        else:
+            datasets = [config.tl_data]
 
-        for set in range(5):
-            print(f'Running on set {set}')
-            config.set = set
-            DataHandler = dh_tl.get_data(config, set)
+        for dataset in datasets:
+            config.tl_data = dataset
+            print(f'Running full simulation for {config.tl_data} dataset')
 
-            #print("TEST1")
-            #print(DataHandler['X_biofeat_train'].shape)
-
-            enzymes = ['multi_task']#['wt', 'esp', 'hf', 'multi_task']
-            train_types = ['full_tl', 'LL_tl', 'gl_tl', 'no_em_tl', 'no_tl', 'no_pre_train']
-            for enzyme in enzymes:
-                config.enzyme = enzyme
-                for train_type in train_types:
-                    if (enzyme in ['esp', 'hf']) and (train_types == 'no_pre_train'):
-                        continue # If there is no pre train data, we dont need to train model for each type of enzyme
-                    # if os.path.exists(f'models/transfer_learning/{config.tl_data}/set{config.set}/DeepHF_old/{config.enzyme}/{train_type}/model_9/model'):
-                    #     continue
-                    print(f'#################### Running {enzyme} with {train_type} model #############################')
-                    config.train_type = train_type
-                    config.epochs = 1000
-                    config.save_model = False
-                    print(f'Running cross_v_HPS for {enzyme} with {train_type} model')
-
-                    if config.train_type == 'no_pre_train':
-                        # hps_tl.tl_param_search(config, DataHandler)
-                        config.epochs = 200
-                        config.batch_size = 190
-                        config.init_lr = 0.027
-                        config.optimizer = SGD
-                        opt_epochs = cv_tl.cross_v_HPS(config, DataHandler)
-
-                        config.epochs = opt_epochs
+            for set in range(5):
+                print(f'Running on set {set}')
+                config.set = set
+                DataHandler = dh_tl.get_data(config, set)
 
 
-                    elif config.train_type == 'no_tl':
-                        config.epochs = 0
-                    else:
-                        opt_epochs = cv_tl.cross_v_HPS(config, DataHandler)
-                        # opt_epochs = 1 # TODO - delete (debug line)
-                        config.epochs = opt_epochs
+                enzymes = ['multi_task']
+                train_types = ['full_tl', 'LL_tl', 'gl_tl', 'no_em_tl', 'no_tl', 'no_pre_train']
+                for enzyme in enzymes:
+                    config.enzyme = enzyme
+                    for train_type in train_types:
+                        if (enzyme in ['esp', 'hf']) and (train_types == 'no_pre_train'):
+                            continue # If there is no pre train data, we dont need to train model for each type of enzyme
+                        # if os.path.exists(f'models/transfer_learning/{config.tl_data}/set{config.set}/DeepHF_old/{config.enzyme}/{train_type}/model_9/model'):
+                        #     continue
+                        print(f'#################### Running {enzyme} with {train_type} model #############################')
+                        config.train_type = train_type
+                        config.epochs = 1000
+                        config.save_model = False
+                        print(f'Running cross_v_HPS for {enzyme} with {train_type} model')
 
-                    print(f'Running full_train for {enzyme} with {train_type} model')
-                    config.save_model = True
-                    mean = cv_tl.train_10(config, DataHandler)
+                        if config.train_type == 'no_pre_train':
+                            # hps_tl.tl_param_search(config, DataHandler)
+                            config.epochs = 200
+                            config.batch_size = 190
+                            config.init_lr = 0.027
+                            config.optimizer = SGD
+                            opt_epochs = cv_tl.cross_v_HPS(config, DataHandler)
 
-                    print(f'Running ensemble for {enzyme} with {train_type} model')
-                    spearmanr = ensemble_util_tl.train_ensemble(config, DataHandler)
+                            config.epochs = opt_epochs
 
-                    testing_util_tl.save_results(config, enzyme, train_type, mean, spearmanr)
-                    keras.backend.clear_session()
+
+                        elif config.train_type == 'no_tl':
+                            config.epochs = 0
+                        else:
+                            opt_epochs = cv_tl.cross_v_HPS(config, DataHandler)
+                            # opt_epochs = 1 # TODO - delete (debug line)
+                            config.epochs = opt_epochs
+
+                        print(f'Running full_train for {enzyme} with {train_type} model')
+                        config.save_model = True
+                        mean = cv_tl.train_10(config, DataHandler)
+
+                        print(f'Running ensemble for {enzyme} with {train_type} model')
+                        spearmanr = ensemble_util_tl.train_ensemble(config, DataHandler)
+
+                        testing_util_tl.save_results(config, enzyme, train_type, mean, spearmanr)
+                        keras.backend.clear_session()
 
     if config.simulation_type == 'postprocess':
         postprocess_tl.postprocess(config)
