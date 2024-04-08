976144bb3e57d152be268cc5c0a22dfd96258e8b
River <tziony.i@gmail.com>
2024-04-08 21:06:05 +0300

    improved graphs

--- final_instructions.md
+++ final_instructions.md
@@ -4,12 +4,10 @@
 # Preprocessing
 DeepHF
 python main.py -s_type preprocessing
-python main_tl.py -s_type preprocessing --tl_data ALL_ORIGINAL_DATA --tl_data_category U6T7 
+python main_tl.py -s_type preprocess--tl_data ALL_ORIGINAL_DATA --tl_data_category U6T7 
 
 CRISPRon
-python main_tl.py -s_type preprocessing --tl_data ALL_ORIGINAL_DATA --tl_data_category U6T7 
-
-
+python main_tl.py -s_type preprocess --tl_data ALL_ORIGINAL_DATA --tl_data_category U6T7 
 
 
 # DeepHF cross-v
@@ -19,16 +17,15 @@ python main.py -s_type cross_v --enzyme hf
 delete results/pretrain
 delete models/pre_train/DeepHF_old/*
 
-
-# DeepHF cross-v no bio features
-
 # DeepHF full-cross-v
+python main.py -s_type cross_v --enzyme multi_task
 
+# DeepHF Fine Tune
+[EXPLAIN WHERE TO PLACE MODELS]
+python main.py -s_type full_sim --tl_data ALL_ORIGINAL_DATA --tl_data_category U6T7 
 
 # CRISPRon Fine Tune
 
 # Tool
 
-# DeepHF Fine Tune
-[EXPLAIN WHERE TO PLACE MODELS]
-python main.py -s_type full_sim --tl_data ALL_ORIGINAL_DATA --tl_data_category U6T7 
+
--- new_version/make_tl_report.py
+++ new_version/make_tl_report.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-MODE = 'CRISPRON'
+MODE = 'VARIOUS'
 
 def main():
     # get all folders in the current directory
@@ -56,50 +56,78 @@ def main():
             print(averages)
             stds = stds.to_dict()
 
+        else:
+            # if folder has a substring with leenay in it, skip it
+            if folder == 'leenay':
+                continue
+            #print(folder)
+            #print(subfolder)
+            df = pd.read_csv(os.path.join(folder, 'results.csv'), index_col=0)
+
+            # make into floats
+            df = df.astype(float)
+            # Make the average of all columns
+            averages = df.mean()
+
+            # Make the std of all columns
+            stds = df.std()
+
+            # make a dictionary for each column to average and std
+            averages = averages.to_dict()
+            #print(averages)
+            stds = stds.to_dict()
+
 
         # make a dataframe from the averages and stds
         df = pd.DataFrame.from_dict(averages, orient='index')
         df.columns = ['Average']
         df['Std'] = stds.values()
 
-        if MODE == 'DEEPCRISTL':
-            df_only_ensemble = df.iloc[[1,3,5,7,9,11], :]
-        else:
-            df_only_ensemble = df.iloc[[1,3,5,7,9], :]
+        # if MODE == 'DEEPCRISTL':
+        #     df_only_ensemble = df.iloc[[1,3,5,7,9,11], :]
+        # elif MODE == 'CRISPRON':
+        #     df_only_ensemble = df.iloc[[1,3,5,7,9], :]
+        # else:
+        #     pass # TODO: here do stuff
 
-        # remove "ensamble" from the row names
-        df_only_ensemble.index = df_only_ensemble.index.str.replace('_ensemble', '')
+        # # remove "ensamble" from the row names
+        # df_only_ensemble.index = df_only_ensemble.index.str.replace('_ensemble', '')
 
-        # make a bar plot of the averages and stds
-        df_only_ensemble.plot.bar(y='Average', yerr='Std', rot=0)
-        # make the plot bigger
-        plt.gcf().set_size_inches(5, 3)
+        # # make a bar plot of the averages and stds
+        # df_only_ensemble.plot.bar(y='Average', yerr='Std', rot=0)
+        # # make the plot bigger
+        # plt.gcf().set_size_inches(5, 3)
 
-        # remove the legend
-        plt.legend().remove()
-        # Color the bars
-        #ax = plt.gca()
-        # ax.patches[0].set_facecolor('r')
-        # ax.patches[1].set_facecolor('g')
-        # ax.patches[2].set_facecolor('b')
-        # ax.patches[3].set_facecolor('y')
-        # ax.patches[4].set_facecolor('c')
-        # #ax.patches[5].set_facecolor('m')
-        #ax.patches[6].set_facecolor('k')
+        # # remove the legend
+        # plt.legend().remove()
+        # # Color the bars
+        # #ax = plt.gca()
+        # # ax.patches[0].set_facecolor('r')
+        # # ax.patches[1].set_facecolor('g')
+        # # ax.patches[2].set_facecolor('b')
+        # # ax.patches[3].set_facecolor('y')
+        # # ax.patches[4].set_facecolor('c')
+        # # #ax.patches[5].set_facecolor('m')
+        # #ax.patches[6].set_facecolor('k')
         
-        plt.title(folder)
-        plt.tight_layout()
-        plt.savefig(folder + '.png')
-        plt.close()
+        # plt.title(folder)
+        # plt.tight_layout()
+        # plt.savefig(folder + '.png')
+        # plt.close()
 
         results_of_all_folders.append(df)
 
     # Combine all the dataframes into one
     df = pd.concat(results_of_all_folders, axis=1)
     column_names = []
-    for folder in folders:
-        column_names.append(folder + ' Average')
-        column_names.append(folder + ' Std')
+    if MODE != 'VARIOUS':
+        for folder in folders:
+            column_names.append(folder + ' Average')
+            column_names.append(folder + ' Std')
+    else:
+        for folder in [f for f in folders if f != 'leenay']:
+            column_names.append(folder + ' Average')
+            column_names.append(folder + ' Std')
     
     df.columns = column_names
        
