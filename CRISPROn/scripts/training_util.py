6f208b5ea698344103887e8e841f1bae1419198d
River <tziony.i@gmail.com>
2024-05-27 16:47:19 +0300

    graphs update

--- CRISPROn/scripts/training_util.py
+++ CRISPROn/scripts/training_util.py
@@ -3,6 +3,10 @@ def train_model(config, DataHandler, model, callback_list, verbose=2, final_mode
         print('Start training')
     train_input, y_train = [DataHandler['X_train'], DataHandler['dg_train']], DataHandler['y_train']
     valid_input, y_val = [DataHandler['X_valid'], DataHandler['dg_valid']], DataHandler['y_valid']
+
+    if not final_models:
+        pre_train_val_loss = model.evaluate(valid_input, y_val, verbose=0)
+        pre_train_train_loss = model.evaluate(train_input, y_train, verbose=0)
     
 
     history = model.fit(train_input,
@@ -18,6 +22,9 @@ def train_model(config, DataHandler, model, callback_list, verbose=2, final_mode
     if not final_models:
         for key in history.history.keys():
             print(key, history.history[key])
+
+        print('Pre train val loss:', pre_train_val_loss)
+        print('Pre train train loss:', pre_train_train_loss)
 
     if return_model:
         return history, model
